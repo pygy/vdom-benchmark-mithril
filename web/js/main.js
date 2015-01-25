28035316a8b60c65bc722384a6d07173e2311a1b
Boris Kaul <localvoid@gmail.com>
2015-01-25 16:01:32 +0600

    Initial commit

---
+++ web/js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var benchmark = require('vdom-benchmark-base');
+var m = require('mithril');
+
+var NAME = 'mithril';
+var VERSION = '0.1.28';
+
+function renderTree(nodes) {
+  var children = [];
+  var i;
+  var e;
+  var n;
+
+  for (i = 0; i < nodes.length; i++) {
+    n = nodes[i];
+    if (n.children !== null) {
+      children.push({tag: 'div', attrs: {key: n.key}, children: renderTree(n.children)});
+    } else {
+      children.push({tag: 'span', attrs: {key: n.key}, children: n.key.toString()});
+    }
+  }
+
+  return children;
+}
+
+function BenchmarkImpl(container, a, b) {
+  this.container = container;
+  this.a = a;
+  this.b = b;
+}
+
+BenchmarkImpl.prototype.setUp = function() {
+};
+
+BenchmarkImpl.prototype.tearDown = function() {
+  m.render(this.container, '', true);
+};
+
+BenchmarkImpl.prototype.render = function() {
+  m.render(this.container, {tag: 'div', children: renderTree(this.a)});
+};
+
+BenchmarkImpl.prototype.update = function() {
+  m.render(this.container, {tag: 'div', children: renderTree(this.b)});
+};
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  benchmark(NAME, VERSION, BenchmarkImpl);
+}, false);
